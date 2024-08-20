836d23a9200cdfc13c7e4fee68143607d47ae1eb
yeray <yeray.programacion.web@gmail.com>
2024-08-20 11:36:19 +0100

    creando estados para guardar las opciones

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState } from 'react';
+
+//** componente Square */
+function Square() {
+
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button className='square'
+      onClick={handleClick}
+    >
+    {value}
+    </button>
+    );
+}
+
+//** componente Board */
+export default function Board() {
+
+  return (
+    <>
+      <div className='board-row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='board-row'>
+        <Square  />
+        <Square  />
+        <Square  />
+      </div>
+      <div className='board-row'>
+        <Square  />
+        <Square  />
+        <Square  />
+      </div>
+    </>
+  );
+}
