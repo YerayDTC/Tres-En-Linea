b7c6313e8449324abba5d2c40d42c2076873bcf3
yeray <yeray.programacion.web@gmail.com>
2024-08-21 15:39:03 +0100

    tomar turnnos para ver a que jugador le toca

--- src/App.js
+++ src/App.js
@@ -10,13 +10,17 @@ function Square({value, onSquareClick}) {
 
 //** componente Board */
 export default function Board() {
-  
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+
+    if(squares[i]) return; //si el cuadrado ya tiene un valor, no hace nada
+
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    xIsNext ? nextSquares[i] = 'X' : nextSquares[i] = 'O'; // if condicional
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
@@ -39,3 +43,29 @@ export default function Board() {
     </>
   );
 }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+}
+
+for(let i = 0; i < lines.length; i++) {
+  const [a, b, c] = lines[i];
+  if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) { // si todos los cuadrados tienen el mismo valor
+    return squares[a];
+  }
+}
+/*Recapitulemos lo que sucede cuando un usuario hace clic en el cuadrado superior izquierdo de su tablero para agregarle una X:
+
+Al hacer clic en el cuadrado superior izquierdo, se ejecuta la función que el button recibe como prop onClick del Square. El componente Square recibe esa función como una prop onSquareClick del Board. El componente Board define esa función directamente en el JSX. Llama a handleClick con un argumento de 0.
+handleClick usa el argumento (0) para actualizar el primer elemento de la matriz squares de null a X.
+El estado squares del componente Board se actualiza, por lo que Board y todos sus elementos secundarios se vuelven a renderizar. Esto hace que la prop value del componente Square con el índice 0 cambie de null a X.
+Al final, el usuario ve que el cuadrado superior izquierdo ha pasado de estar vacío a tener una X después de hacer clic en él. */
